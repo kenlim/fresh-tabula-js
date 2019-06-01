260004b5c613b2becf221e153993afb38c9f656f
cdtinney <colintinney@gmail.com>
2019-06-01 19:08:28 -0400

    tests: add stubs

--- src/__tests__/Tabula.test.js
+++ src/__tests__/Tabula.test.js
@@ -1,8 +1,22 @@
 const Tabula = require('../Tabula');
 
 describe('Tabula', () => {
-  it('creates a new instance when using new()', () => {
-    const table = new Tabula('foo', {});
-    expect(table instanceof Tabula).toBeTruthy();
+  describe('constructor', () => {
+    it('creates a new instance when using new()', () => {
+      const table = new Tabula('foo', {});
+      expect(table instanceof Tabula).toBeTruthy();
+    });
+  });
+
+  describe('streamCsv()', () => {
+    // TODO
+  });
+
+  describe('extractStreamCsv()', () => {
+    // TODO
+  });
+
+  describe('extractCsv()', () => {
+    // TODO
   });
 });
